42a7441f7768c875007f7e7ed7abecca872b4cd8
Tushar Rohilla <tushar.rohilla@example.com>
2025-07-13 13:51:22 +0530

    Fix CORS URLs for production deployment

--- backend/api/index.js
+++ backend/api/index.js
@@ -17,7 +17,8 @@ app.use(cors({
     "http://localhost:5174", 
     "http://localhost:5175",
     "https://food-mania-henna.vercel.app",
-    "https://food-mania-admin-nine.vercel.app"
+    "https://food-mania-admin-nine.vercel.app",
+    
   ],
   credentials: true
 }));
